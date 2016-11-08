64529d08a159c5fa8f927eeafef2c7077b8028ae
Ryan Olson <rmolson@gmail.com>
2016-11-08 01:56:33 -0700

    updating templates

--- {{cookiecutter.project_name}}/version.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-openai = datetime(2016, 8, 15, 0, 0, 0)
-version = datetime.now() - openai
-
-#print "{0}.{1:05d}".format(version.days, version.seconds)
-print version.days
